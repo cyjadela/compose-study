446f46f28f3d4b712413afcf9a9795f3b7264352
cyjadela <adelacho@naver.com>
2024-08-29 16:06:30 +0900

    Feat: MyApp Composable로 대체

--- compose_start/app/src/main/java/com/android/compose_start/MainActivity.kt
+++ compose_start/app/src/main/java/com/android/compose_start/MainActivity.kt
@@ -19,13 +19,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Compose_startTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
