0c2ece3ac8b6a001329c1cce25279fccac3793ce
cyjadela <adelacho@naver.com>
2024-08-29 16:44:29 +0900

    Feat: remember로 클릭 시 상태 변경

--- compose_start/app/src/main/java/com/android/compose_start/MainActivity.kt
+++ compose_start/app/src/main/java/com/android/compose_start/MainActivity.kt
@@ -6,13 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,6 +32,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val expanded = remember { mutableStateOf(false) }
+
     Surface(color = MaterialTheme.colorScheme.primary,
     modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
         Row(modifier = Modifier.padding(24.dp)) {
@@ -38,8 +41,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 Text(text = "Hello ")
                 Text(text = name)
             }
-            ElevatedButton(onClick = { /*TODO*/ }) {
-                Text("더보기")
+            ElevatedButton(onClick = {
+                expanded.value = !expanded.value
+            }) {
+                Text(if (expanded.value) "Show less" else "Show more")
             }
         }
     }
