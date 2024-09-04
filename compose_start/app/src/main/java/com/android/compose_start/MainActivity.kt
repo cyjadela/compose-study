e448d201709a5fda70ed449aced92b605701a4ae
cyjadela <adelacho@naver.com>
2024-09-04 09:08:03 +0900

    Feat: Greeting을 Card 형태로 구현

--- compose_start/app/src/main/java/com/android/compose_start/MainActivity.kt
+++ compose_start/app/src/main/java/com/android/compose_start/MainActivity.kt
@@ -4,8 +4,8 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -14,12 +14,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.Icons.Filled
 import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -28,7 +28,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -51,45 +50,101 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+private fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ){
+            CardContent(name)
+    }
+}
+
+@Composable
+private fun CardContent(name: String) {
     var expanded by rememberSaveable { mutableStateOf(false) }
 
-    val extraPadding by animateDpAsState(
-        if (expanded) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
-
-    Surface(color = MaterialTheme.colorScheme.primary,
-    modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
                 .weight(1f)
-                .padding(bottom = extraPadding.coerceAtLeast(0.dp))) { // padding이 음수가 되면 앱이 다운될 수 있으므로 0으로 설정
-                Text(text = "Hello ")
-                Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(
-                    fontWeight = FontWeight.ExtraBold,
-                    color = MaterialTheme.colorScheme.onPrimary
-                ))
-            }
-            IconButton(onClick = {
-                expanded = !expanded
-            }
-            ) {
-                Icon(
-                    imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
-                    contentDescription = if (expanded) {
-                        stringResource(R.string.show_less)
-                    } else {
-                        stringResource(R.string.show_more)
-                    }
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                fontWeight = FontWeight.ExtraBold
+            )
+            )
+            if (expanded) {
+                Text(
+                    text = ("Compose Study ! ").repeat(10)
                 )
             }
         }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Filled.ExpandLess else Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+            )
+        }
     }
 }
+
+//
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    var expanded by rememberSaveable { mutableStateOf(false) }
+//
+//    val extraPadding by animateDpAsState(
+//        if (expanded) 48.dp else 0.dp,
+//        animationSpec = spring(
+//            dampingRatio = Spring.DampingRatioMediumBouncy,
+//            stiffness = Spring.StiffnessLow
+//        )
+//    )
+//
+//    Surface(color = MaterialTheme.colorScheme.primary,
+//    modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+//        Row(modifier = Modifier.padding(24.dp)) {
+//            Column(modifier = Modifier
+//                .weight(1f)
+//                .padding(bottom = extraPadding.coerceAtLeast(0.dp))) { // padding이 음수가 되면 앱이 다운될 수 있으므로 0으로 설정
+//                Text(text = "Hello ")
+//                Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(
+//                    fontWeight = FontWeight.ExtraBold,
+//                    color = MaterialTheme.colorScheme.onPrimary
+//                ))
+//            }
+//            IconButton(onClick = {
+//                expanded = !expanded
+//            }
+//            ) {
+//                Icon(
+//                    imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+//                    contentDescription = if (expanded) {
+//                        stringResource(R.string.show_less)
+//                    } else {
+//                        stringResource(R.string.show_more)
+//                    }
+//                )
+//            }
+//        }
+//    }
+//}
 
 //@Composable
 //fun MyApp(
@@ -108,7 +163,7 @@ fun MyApp(modifier: Modifier = Modifier) {
 
     var shouldShowOnboarding by rememberSaveable { mutableStateOf(true)}
 
-    Surface(modifier) {
+    Surface(modifier, color = MaterialTheme.colorScheme.background) {
         if (shouldShowOnboarding) {
             OnboardingScreen( onContinueClicked = { shouldShowOnboarding = false })
         } else {
